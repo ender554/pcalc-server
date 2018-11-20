ba643bc28e7629f95d4a9b42206cb6c509adf896
Joshua Adams <joshadams554@gmail.com>
2018-11-20 09:39:09 -0500

    splitting repos

---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const passport = require('passport');
+const { PORT, CLIENT_ORIGIN } = require('./config');
+const { dbConnect } = require('./db-mongoose');
+const localStrategy = require('./passport/local');
+const jwtStrategy = require('./passport/jwt');
+// const {dbConnect} = require('./db-knex');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+const app = express();
+
+app.use(
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
+);
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+app.use(express.json());
+
+// const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+
+app.use('/api/users', usersRouter);
+app.use('/api/auth', authRouter);
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+function runServer(port = PORT) {
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
+}
+
+if (require.main === module) {
+  dbConnect();
+  runServer();
+}
+
+module.exports = { app };
